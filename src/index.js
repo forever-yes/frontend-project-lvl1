88c2d81fb3a22c5105a7b8b1c13cb48201270849
forever-yes <molotov.eugene@gmail.com>
2022-06-17 00:07:36 +0500

    add game's engine

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+import greeting from './cli.js';
+
+const play = (game, message) => {
+  const name = greeting();
+  console.log(message);
+
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = game();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default play;
