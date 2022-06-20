d3c2278280b0aa3050dc0e93e2506212a815d362
forever-yes <molotov.eugene@gmail.com>
2022-06-20 21:44:24 +0500

    fix logic

--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
 import readlineSync from 'readline-sync';
 
 import greeting from './cli.js';
-import startCalcGame from './games/calc.js';
-import startEvenGame from './games/even.js';
-import startGcdGame from './games/gcd.js';
-import startPrimeGame from './games/prime.js';
-import startProgressionGame from './games/progression.js';
 
-const play = (gameName) => {
-  let game = () => {};
-  switch (gameName) {
-    case ('brain-calc'):
-      game = startCalcGame;
-      break;
-    case ('brain-even'):
-      game = startEvenGame;
-      break;
-    case ('brain-gcd'):
-      game = startGcdGame;
-      break;
-    case ('brain-prime'):
-      game = startPrimeGame;
-      break;
-    case ('brain-progression'):
-      game = startProgressionGame;
-      break;
-    default:
-      console.log('wtf?');
-  }
-
-  const [message] = game();
-  const name = greeting();
-  console.log(message);
-  const attempts = 3;
+const play = (game, gameRule, attempts = 3) => {
+  const name = greeting(gameRule);
 
   for (let i = 0; i < attempts; i += 1) {
-    const [question, answer] = game().slice(1);
+    const [question, answer] = game();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === answer) {
+    if (userAnswer === String(answer)) {
       console.log('Correct!');
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'`);
