186ba6aa1a6da75bfb9c9bc2c96944a68c35ed08
forever-yes <molotov.eugene@gmail.com>
2022-06-20 21:44:50 +0500

    add new functionality

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,12 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
+const greeting = (gameRule) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  if (gameRule) {
+    console.log(gameRule);
+  }
   return name;
 };
 
