708aa67a424f1259fa971f516e1babbcfba1b7cf
forever-yes <molotov.eugene@gmail.com>
2022-06-22 19:24:03 +0500

    fix logic

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const greeting = (gameRule) => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  if (gameRule) {
-    console.log(gameRule);
-  }
   return name;
 };
 
